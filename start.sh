e4c845ae8f901655828207a3aeb677385ee19959
Jean Carlo EM <jeancarlo@jeancarloem.com>
2023-08-31 20:11:08 -0300

    echo running self node

--- start.sh
+++ start.sh
@@ -13,6 +13,7 @@ cd dero_linux_amd64
 
 # RUN FULL NODE
 if [[ "$DAEMON" =~ ^\s*((ftp|http)s?)?(localhost|127.0.0.1) ]]; then
+  echo "Running SELF NODE"
   sudo ./derod-linux-amd64 --fastsync --integrator-address=$WALLE --rpc-bind=127.0.0.1:10100 --data-dir=/data &
 fi
 
